3f201b69258da192fe36a12984c0e5b4f2d868b5
Manius-Laptop\Maniuś <najwer23@gmail.com>
2018-09-30 17:21:34 +0200

    blog - back-end

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,16 +5,136 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Posts;
+
 class BlogController extends AbstractController
 {
+
+     public function getPosts($min,$max) 
+    {
+        // znajdz posty od najnowszego do najstarszego
+        $query=$this->getDoctrine()
+        ->getRepository(Posts::class)->createQueryBuilder('p')
+        ->select('p.id', 'p.date', 'p.topic')
+        ->setFirstResult($min)
+        ->setMaxResults($max)
+        ->orderBy('p.date', 'DESC')
+        ->getQuery();
+        $posts = $query->execute();
+        return $posts;
+    }
+
+    public function numberOfPosts()
+    {
+        // liczba postow
+        $qcount = $this->getDoctrine()->getManager();
+        $count = $qcount->createQuery(
+        ' SELECT  count(p.id)
+          FROM    App\Entity\Posts p
+        ')
+        ->getSingleScalarResult();
+        return $count;
+    }
+
+    public function page()
+    {
+        // ilosc wynikow na stronie 
+        $page=1;
+        return $page;
+    }
+
+
+    
+
     /**
-     * @Route("/blog", name="blog")
-     */
+    * @Route("/blog", name="blog")
+    */
     public function showBlog()
     {
-        return $this->render('firstPageBlog/firstPageBlog.html.twig', [ 
-            'namePage' => 'Syssitia App - blog',
-             
+        //liczba postów
+        $numberOfPosts=$this->numberOfPosts();
+
+        // liczba elementow na stronie
+        $page=$this->page();
+        $numberOfPages=ceil($numberOfPosts/$page);
+
+        // znajdz posty od najnowszego do najstarszego
+        $posts=$this->getPosts(0,$page);
+
+        return $this->render('firstPageBlog/firstPageBlog.html.twig', [
+            'posts'=> $posts,
+            'nav' => '1',
+            'namePage' => 'Syssitia App - Blog',
+            'numberOfPages' => $numberOfPages,
         ]);
+
     }
+
+
+    /**
+     * @Route("/blog/posts/{slug}", name="posts")
+     */
+    public function showPost($slug)
+    {
+        // znajdz post o danym id
+        $post = $this->getDoctrine()->getRepository(Posts::class)->find($slug);
+
+        // inkrementuj odwiedziny
+        $hits=$post->getHits();
+        $hits=$hits+1;
+
+        // zapisz odwiedziny do bazy danego posta
+        $entityManager = $this->getDoctrine()->getManager(); 
+        $post->setHits($hits);
+        $entityManager->flush();
+
+        //pobierz info o poscie i ustaw zmienne
+        $topic=$post->getTopic();
+        $date=$post->getDate();        
+        $path = 'firstPage/posts/post'.$slug.'.html.twig';
+
+        return $this->render('firstPageBlog/basePostBlog.html.twig', [
+               'path' => $path,
+               'hits' => $hits,
+               'topic' => $topic,
+               'date' => $date,
+               'idPost' => $slug,
+               
+               'namePage' => 'Syssitia App - Blog - '.$topic,
+               'undo' => '2',
+               'menuBlog' => '1',
+               'nav' => '1'
+           ]);
+    }
+
+    
+    /**
+     * @Route("/blog/page/{slug}", name="showPage")
+     */
+    public function showPage($slug)
+    {
+        $prevPage=$slug-1;
+        $nextPage=$slug+1;
+
+        //liczba postรณw
+        $numberOfPosts=$this->numberOfPosts();
+
+        // liczba elementow na stronie
+        $page=$this->page();
+        $numberOfPages=ceil($numberOfPosts/$page);
+        
+        // znajdz posty od najnowszego do najstarszego
+        $posts=$this->getPosts(($slug-1)*$page,$page);
+
+         return $this->render('firstPageBlog/firstPageBlog.html.twig', [
+            'posts'=> $posts,
+            'nav' => '1',
+            'namePage' => 'Syssitia App - Blog - strona '.$slug,
+            'numberOfPages' => $numberOfPages,
+            'prevPage' => $prevPage,
+            'nextPage' => $nextPage,
+        ]);
+
+    }
+   
 }
---
+++ src/Entity/Posts.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostsRepository")
+ */
+class Posts
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $topic;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $hits;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTopic(): ?string
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(string $topic): self
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    public function getHits(): ?int
+    {
+        return $this->hits;
+    }
+
+    public function setHits(?int $hits): self
+    {
+        $this->hits = $hits;
+
+        return $this;
+    }
+}
